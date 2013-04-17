f99f94022ddd8f45d51a122d8b16fc3916fd6811
David Blurton <david.blurton@red-gate.com>
2013-04-17 15:49:41 +0100

    Add some tests

---
+++ ClickCounterTests/BasicTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace ClickCounterTests
+{
+    public class BasicTests
+    {
+        [Test]
+        public void ThisTestShouldPass()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void ThisTestShouldFail()
+        {
+            Assert.Fail("This was meant to fail :)");
+        }
+    }
+}
